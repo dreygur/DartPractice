6bbfe6fffa98b4fbb576c9c196b6812141fe112b
Rakibul Yeasin <rytotul@gmail.com>
2019-05-09 14:08:01 +0600

    Studied If...else

--- Functions.dart
+++ Functions.dart
@@ -3,6 +3,7 @@ var name = 'Dart';
 void callName(String name) {
   print('Hello $name, How are you?');
   print('Hello ${name.toUpperCase()}, You are funny!');
+  print('Hello ${name.toLowerCase()}, You are funny!');
 }
 
 void main() {
---
+++ Using If_else.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+var name = 'Dart';
+
+void callName(String name) {
+  print('Hello $name, How are you?');
+  print('Hello ${name.toUpperCase()}, You are funny!');
+  print('Hello ${name.toLowerCase()}, You are funny!');
+}
+
+void main() {
+  var userInput = stdin.readLineSync();
+  if (userInput == '') {
+    callName(userInput);
+  }
+  else if (userInput == 'Honululu') {
+    var name = 'I am not aware of what I am douing';
+    callName(name);
+  } else {
+    callName(name);
+  }
+}
